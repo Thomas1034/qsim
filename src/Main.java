f347768e1d29d39f97436658ae80a7078fbf23ec
Thomas Kern <142808316+Thomas1034@users.noreply.github.com>
2024-03-01 13:06:06 -0500

    Renamed the documentation folder

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import squire.test.QuantumGateTests;
 
+/**
+ * Entry point to the program, primarily intended for testing.
+ */
 public class Main {
 
+	/**
+	 * Entry point to the program, primarily intended for testing.
+	 * 
+	 * @param args Command line arguments.
+	 */
 	public static void main(String[] args) {
 		QuantumGateTests.run();
 	}
